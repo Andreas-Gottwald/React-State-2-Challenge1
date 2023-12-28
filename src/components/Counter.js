f0c9ac4d663e4ca3b565ea70767fc31e9544dfd4
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-28 16:48:34 +0100

    finish

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [increment, setIncrement] = useState(0);
+
+  function handlePlusClick() {
+    setIncrement(increment + 1);
+  }
+
+  function handleMinusClick() {
+    setIncrement(increment - 1);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={increment} />
       <div className="counter__buttons-container">
         <button
+          onClick={handlePlusClick}
           type="button"
           className="counter__button"
           aria-label="increment count"
@@ -14,6 +25,7 @@ export default function Counter() {
           +
         </button>
         <button
+          onClick={handleMinusClick}
           type="button"
           className="counter__button"
           aria-label="decrement count"
